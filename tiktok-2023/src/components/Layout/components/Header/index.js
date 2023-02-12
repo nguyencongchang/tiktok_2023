9b2f469811da39af55c1f1fe313fc00287773e09
Chang Nguyen <nguyenchang1999@gmai.com>
2023-02-12 22:47:56 +0700

    build header #1

--- tiktok-2023/src/components/Layout/components/Header/index.js
+++ tiktok-2023/src/components/Layout/components/Header/index.js
@@ -1,11 +1,28 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/asssets/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img alt="TikTok" src={images.logo} />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck="false"></input>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
