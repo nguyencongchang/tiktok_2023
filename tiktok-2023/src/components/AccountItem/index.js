2166e311929662814a903aef5b71af76c6cd07f3
Chang Nguyen <nguyenchang1999@gmai.com>
2023-03-18 20:31:10 +0700

    proptypes

--- tiktok-2023/src/components/AccountItem/index.js
+++ tiktok-2023/src/components/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
@@ -19,5 +20,9 @@ function AccountItem({ data }) {
         </Link>
     );
 }
+
+AccountItem.protoTypes = {
+    data: PropTypes.object,
+};
 
 export default AccountItem;
--- tiktok-2023/src/components/Button/index.js
+++ tiktok-2023/src/components/Button/index.js
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 import styles from './Button.module.scss';
 import { Link } from 'react-router-dom';
 const cx = classNames.bind(styles);
@@ -62,5 +63,9 @@ function Button({
         </Comp>
     );
 }
+
+Button.propTypes = {
+    children: PropTypes.node,
+};
 
 export default Button;
